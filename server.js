c8e7d81f4ec308742ecad9dd34e7f957b3d8829a
Gustavo <gustavo.m.netto@outlook.com>
2016-04-28 15:39:22 -0400

    change connection

--- server.js
+++ server.js
@@ -29,18 +29,18 @@ var MySQLStore = require('express-mysql-session')(session);
 
 
 db_config = {
-    host: 'us-cdbr-iron-east-03.cleardb.net',
-    user: 'b56fefe00420cd',
-    password: '6e81455e',
-    database: 'heroku_6d29352a6951ad0'
+    host: process.env.CLEARDB_DATABASE_HOST,
+    user: process.env.CLEARDB_DATABASE_USER,
+    password: process.env.CLEARDB_DATABASE_PASSWORD,
+    database: process.env.CLEARDB_DATABASE_SCHEMA
 }
 
 var options_session = {
-    host: 'us-cdbr-iron-east-03.cleardb.net',
+    host: process.env.CLEARDB_DATABASE_HOST,
     port: 3306,
-    user: 'b56fefe00420cd',
-    password: '6e81455e',
-    database: 'heroku_6d29352a6951ad0',
+    user: process.env.CLEARDB_DATABASE_USER,
+    password: process.env.CLEARDB_DATABASE_PASSWORD,
+    database: process.env.CLEARDB_DATABASE_SCHEMA,
     checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds.
     expiration: 86400000,// The maximum age of a valid session; milliseconds.
     createDatabaseTable: true,// Whether or not to create the sessions database table, if one does not already exist.
