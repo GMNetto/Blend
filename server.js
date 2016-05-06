07300a2603c9a77fb2c4140d1ef4a7031a558a26
Gustavo <gustavo.m.netto@outlook.com>
2016-05-05 20:54:17 -0400

    Testing session

--- server.js
+++ server.js
@@ -98,14 +98,14 @@ if(process.env.PRODUCTION != undefined || run_local == 1){
 var connection;
 var sessionStore;
 console.log(db_config);
+connection = mysql.createConnection(db_config);
+
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config);
   connection.connect(function(err) {
     if(err) {               
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000);
     }else{
-      sessionStore = new MySQLStore(options_session, connection);
       console.log("CONNECTED")      
     }                                    
   });                                    
@@ -124,7 +124,7 @@ function handleDisconnect() {
 handleDisconnect();
 
 
-
+sessionStore = new MySQLStore(options_session, connection);
 //var sessionStore = new MySQLStore(options_session, connection);
 app.use(session({
     name: 'server-session-cookie-id',
