ad7113622741573b6e50db2eb22aac5751b545c7
sch3 <steven_ho@brown.edu>
2016-05-10 20:59:50 -0400

    Fixed feedback date system

--- static/feedback.js
+++ static/feedback.js
@@ -7,9 +7,13 @@ window.addEventListener('load', function(){
 
 function populateDateField(duration){
     //http://stackoverflow.com/questions/15910761/add-5-days-to-the-current-date-using-javascript
-    var curdate = new Date(initial_date);
-    document.getElementById("start_date").innerHTML = "start date: "+curdate.toDateString();
-    var enddate = new Date(curdate);
+    var array = document.getElementById("start_date").innerHTML.split(" ");
+     var datestring = array[3]+ " "+ array[4]+ " ,"+array[5];
+     console.log(new Date(datestring));
+     console.log(datestring);
+     var curdate = new Date(datestring);
+     var enddate = new Date(curdate);
+     console.log(curdate);
     if(duration==1){
         //console.log("hour");
         //console.log(curdate.getTime());
@@ -36,7 +40,10 @@ function populateDateField(duration){
     //http://stackoverflow.com/questions/2554149/html-javascript-change-div-content
     //document.getElementById("start_date").innerHTML = "start date:"+curdate;
     //console.log(enddate.toDateString());
-    document.getElementById("end_date").innerHTML = "end date:"+enddate.toDateString();
+    var finalcur = curdate.toDateString().split(" ");
+    var finalend = enddate.toDateString().split(" ");
+     document.getElementById("start_date").innerHTML = "start date:"+finalcur[1]+" "+finalcur[2];
+    document.getElementById("end_date").innerHTML = "end date:"+finalend[1]+" "+finalend[2];
 }
 function meta(name) {
     var tag = document.querySelector('meta[name=' + name + ']');
